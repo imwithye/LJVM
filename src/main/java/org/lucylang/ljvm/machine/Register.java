4050401ca3f33a4901ff7082f8e3ed774a7fe47c
Ciel <imwithye@gmail.com>
2016-01-17 22:56:38 +0800

    add Register

--- src/main/java/org/lucylang/ljvm/machine/Register.java
+++ src/main/java/org/lucylang/ljvm/machine/Register.java
@@ -1,4 +1,20 @@
 package org.lucylang.ljvm.machine;
 
+import org.lucylang.ljvm.value.Value;
+
 public class Register {
+    private Value value;
+
+    public Register(Value value) {
+        this.value = value;
+    }
+
+    public Value getValue() {
+        return this.value;
+    }
+
+    public Value assignValue(Value value) {
+        this.value = value;
+        return this.value;
+    }
 }
