f36669ab07eb26cfebf7cbee64df254a8262d24e
Ciel <imwithye@gmail.com>
2016-02-12 13:14:59 +0800

    add module gen

---
+++ src/main/java/org/lucylang/ljvm/generator/ModuleCodeGenerator.java
@@ -0,0 +1,9 @@
+package org.lucylang.ljvm.generator;
+
+import org.lucylang.ljvm.machine.module.Module;
+
+public class ModuleCodeGenerator {
+    public Module visitModule(org.lucylang.ljvm.node.Module module) {
+        return null;
+    }
+}
---
+++ src/main/java/org/lucylang/ljvm/node/Module.java
@@ -0,0 +1,7 @@
+package org.lucylang.ljvm.node;
+
+import java.util.ArrayList;
+
+public class Module {
+    private ArrayList<Function> functions;
+}
